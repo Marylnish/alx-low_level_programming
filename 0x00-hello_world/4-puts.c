9088976fe23e6b2d8dd6b51eedc6cf139ba331ac
Marylnish <kinyuamarie30@gmail.com>
2022-06-03 00:15:45 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ *  main -entry point
  *
- * Return: Always return 0 (Success)
+ *  Return: 0 after printing the function
  */
+
+int main(void)
 {
-	puts("\"Programming is like building a multingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
